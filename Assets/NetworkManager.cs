164a9c4f24108a7d4976ed25259478df7f0ff425
KihtrakRaknas <karthikandsankar@gmail.com>
2020-04-21 20:11:40 -0400

    added photon + basic functions

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager instance;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            this.gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ChangedScene (string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
