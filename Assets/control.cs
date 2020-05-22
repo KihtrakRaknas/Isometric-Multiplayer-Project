e9af56cee880c2274be8db116f5e7c5814449dc5
KihtrakRaknas <karthikandsankar@gmail.com>
2020-05-22 18:54:47 -0400

    moved movement code to fixed update

--- Assets/control.cs
+++ Assets/control.cs
@@ -21,50 +21,52 @@ public class control : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (canShoot && Input.GetKey("space"))
+        {
+            canShoot = false;
+            Invoke("resetShootBool", .2f);
+            print("space");
+            NetworkManager.instance.spawnBullet(this.transform.position, transform.rotation);
+            //clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+        if (Input.GetKey("down"))
+        {
+            xscale += -1;
+        }
+        else if (Input.GetKey("up"))
+        {
+            xscale += 1;
+        }
+        else
+        {
+            xscale /= 1.02f;
+        }
+        if (Input.GetKey("left"))
+        {
+            print("left");
+            zscale += 1;
+        }
+        else if (Input.GetKey("right"))
+        {
+            print("right");
+            zscale += -1;
+        }
+        else
+        {
+            zscale /= 1.02f;
+        }
+        if (Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 0.001)
+                this.GetComponent<Rigidbody>().velocity = new Vector3(0, 10, 0);
+        }
+    }
     void Update()
     {
         if (isMain)
         {
-            if (canShoot && Input.GetKey("space"))
-            {
-                canShoot = false;
-                Invoke("resetShootBool", .2f);
-                print("space");
-                NetworkManager.instance.spawnBullet(this.transform.position, transform.rotation);
-                //clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * 10);
-            }
-            if (Input.GetKey("down"))
-            {
-                xscale += -1;
-            }
-            else if (Input.GetKey("up"))
-            {
-                xscale += 1;
-            }
-            else
-            {
-                xscale /= 1.02f;
-            }
-            if (Input.GetKey("left"))
-            {
-                print("left");
-                zscale += 1;
-            }
-            else if (Input.GetKey("right"))
-            {
-                print("right");
-                zscale += -1;
-            }
-            else
-            {
-                zscale /= 1.02f;
-            }
-
-            if (Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl))
-            {
-                if (Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 0.001)
-                    this.GetComponent<Rigidbody>().velocity = new Vector3(0, 10, 0);
-            }
             var x = this.transform.position.x;
             var y = this.transform.position.y;
             var z = this.transform.position.z;
