e170e8c410dd62a6f9e3ae5032cfdaccda1e3775
ghautham <ghautham.sam01@gmail.com>
2020-05-28 13:42:40 -0400

    added winnerText

---
+++ Assets/displayWinner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using static control;
+
+
+public class displayWinner : MonoBehaviourPun
+
+{
+    // Start is called before the first frame update
+    public Text winText;
+    public bool isMain;
+    string winner;
+    void Start()
+    {
+        isMain = GetComponent<control>().isMain;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        winner = GameObject.FindGameObjectsWithTag("playerObj")[0].GetComponent<PhotonView>().Owner.NickName;
+        if (isMain)
+            winner = "you";
+        if (winner != "")
+            winText.text = winner + " wins!";
+    }
+}
